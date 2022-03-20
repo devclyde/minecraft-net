da481464cee40ffead03337e3daa2d7327363125
devclyde <israel.devclyde@protonmail.com>
2022-03-20 20:25:24 -0300

    feat: initial commit

---
+++ resources/ResourceAssembly.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace minecraft.Application.Resources;
+
+public static class ResourceAssembly
+{
+    public static Assembly Assembly => typeof(ResourceAssembly).Assembly;
+}
